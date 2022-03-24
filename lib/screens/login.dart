6b967be184f5b0c4beb2a11b87eada98d35aa361
Filipe Zanelato <filipezanelato23@gmail.com>
2022-03-24 17:08:12 -0300

    Refactoring login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -14,95 +14,100 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF28D484),
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.fromLTRB(10, 100, 10, 0),
-                  child: Image.asset(
-                    "assets/images/logo.png",
-                    width: 120,
-                    height: 120,
-                  )),
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-                child: const Text(
-                  'Brota ai',
-                  style: TextStyle(
-                      fontStyle: FontStyle.normal,
-                      fontFamily: 'EastSeaDokdo',
-                      fontWeight: FontWeight.w500,
-                      fontSize: 100,
-                      color: Colors.white),
-                ),
+        backgroundColor: const Color(0xFF198754),
+        body: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    margin: const EdgeInsets.only(top: 160.0),
+                    padding: const EdgeInsets.all(10.0),
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      "assets/images/logo-pin.png",
+                      width: 80,
+                      height: 120,
+                    )),
+              ],
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.fromLTRB(11, 0, 11, 50),
+              child: const Text(
+                'Brota ai',
+                style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontFamily: 'EastSeaDokdo',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 70,
+                    color: Colors.white),
               ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: TextField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Senha',
-                  ),
-                ),
-              ),
-              // TextButton(
-              //   onPressed: () {
-              //     //forgot password screen
-              //   },
-              //   child: const Text(
-              //     'Esqueci minha senha',
-              //     style: TextStyle(color: Color(0xFF198754)),
-              //   ),
-              // ),
-              Container(
-                  height: 60,
-                  padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF198754), // Background color
-                    ),
-                    child: const Text('Entrar'),
-                    onPressed: () {
-                      print(emailController.text);
-                      print(passwordController.text);
-                    },
-                  )),
-              Row(
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(11, 42, 11, 42),
+              child: Column(
                 children: <Widget>[
-                  const Text('Não tem conta?'),
-                  TextButton(
-                    child: const Text(
-                      'Cadastre-se',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
                       ),
                     ),
-                    onPressed: () {
-                      //signup screen
-                    },
-                  )
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: TextField(
+                      obscureText: true,
+                      controller: passwordController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Senha',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 60,
+                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF198754), // Background color
+                      ),
+                      child: const Text('Entrar'),
+                      onPressed: () {
+                        print(emailController.text);
+                        print(passwordController.text);
+                      },
+                    ),
+                  ),
                 ],
-                mainAxisAlignment: MainAxisAlignment.center,
               ),
-            ],
-          ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white.withOpacity(0.4),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                const Text('Não tem conta?'),
+                TextButton(
+                  child: const Text(
+                    'Cadastre-se',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    //signup screen
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ],
         ),
       ),
     );
