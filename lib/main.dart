b400cfd1f5effb1a97a741ba1f0a54e2f0fda4e8
Filipe Neves <filipe.nevser@gmail.com>
2022-03-27 11:07:58 -0300

    add named routes and removing junk code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:brota_ai_app/screens/home.dart';
+import 'package:brota_ai_app/screens/login.dart';
 import 'package:brota_ai_app/screens/my_splash_screen.dart';
+import 'package:brota_ai_app/screens/signup.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,52 +21,11 @@ class MyApp extends StatelessWidget {
       home: const Scaffold(
         body: MySplashScreen(),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      routes: {
+        LoginScreen.id: (context) => LoginScreen(),
+        SignUpScreen.id: (context) => SignUpScreen(),
+        HomeScreen.id: (context) => HomeScreen(),
+      }
     );
   }
 }
