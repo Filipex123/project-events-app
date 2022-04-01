792bb4a1674148f6605a8ae3867e6a1f91e72658
Filipe Neves <filipe.nevser@gmail.com>
2022-04-01 00:11:18 -0300

    adding drawer component

---
+++ lib/components/drawer.dart
@@ -0,0 +1,66 @@
+import 'package:brota_ai_app/components/confirm_modal.dart';
+import 'package:brota_ai_app/screens/login.dart';
+import 'package:brota_ai_app/services/token_storage_service.dart';
+import 'package:flutter/material.dart';
+
+class DrawerMenu extends StatelessWidget {
+  const DrawerMenu({
+    Key? key,
+  }) : super(key: key);
+
+  void _logout(BuildContext context) {
+    TokenStorageService.clear();
+    Navigator.pushReplacementNamed(context, LoginScreen.id);
+  }
+
+  void _handleOnClickLogout(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => 
+        ConfirmModal(
+          modalTitle: "Sair",
+          message: "Deseja realmente sair da aplicação?",
+          cancelButtonText: 'CANCELAR',
+          confirmButtonText: 'SAIR',
+          confirmCallback: () { _logout(context); },
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        
+        child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Sair'),
+            onTap: () {
+              _handleOnClickLogout(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
