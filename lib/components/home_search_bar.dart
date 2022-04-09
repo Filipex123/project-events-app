2330cd991f76fc3efff71a28f862d385d37e0d28
Filipe Neves <filipe.nevser@gmail.com>
2022-04-09 01:47:29 -0300

    adding search bar on home

---
+++ lib/components/home_search_bar.dart
@@ -0,0 +1,63 @@
+import 'package:brota_ai_app/components/paleta.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    Key? key,
+    this.inputAction,
+    this.maxLength,
+    required this.handleOnTap,
+    this.controller,
+  }) : super(key: key);
+
+  final TextInputAction? inputAction;
+  final int? maxLength;
+  final Function() handleOnTap;
+  final TextEditingController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return TextFormField(
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black.withOpacity(0.4),
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.circular(50),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Color(0xFFD6822C),
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.circular(50),
+        ),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Icon(
+            FontAwesomeIcons.search,
+            size: 22,
+            color: Colors.black.withOpacity(0.3),
+          ),
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        hintText: 'Pesquisar Eventos',
+        hintStyle: TextStyle(
+          fontSize: 22,
+          fontFamily: 'ABeeZee',
+          color:Colors.black.withOpacity(0.3),
+        ),
+      ),
+      style: const TextStyle(
+        fontSize: 22,
+        fontFamily: 'ABeeZee',
+        color: Colors.black,
+      ),
+      onTap: handleOnTap,
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,17 +2,46 @@ import 'dart:developer';
 
 import 'package:brota_ai_app/components/drawer.dart';
 import 'package:brota_ai_app/components/google-map.dart';
+import 'package:brota_ai_app/components/home_search_bar.dart';
 import 'package:brota_ai_app/components/snapping-sheet/snapping-sheet-widget.dart';
 import 'package:flutter/material.dart';
+import 'package:snapping_sheet/snapping_sheet.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String id = 'home_screen';
+
+  final snappingSheetController = SnappingSheetController();
+
+  void _handleOnTapSearhBar() {
+    if(snappingSheetController.isAttached) {
+      const position = SnappingPosition.factor(
+      positionFactor: 0.0,
+      snappingCurve: Curves.ease,
+      snappingDuration: Duration(milliseconds: 0),
+      grabbingContentOffset: GrabbingContentOffset.top,
+    );
+      snappingSheetController.snapToPosition(position);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SimpleSnappingSheet(
-        widgetBackground: GoogleSimpleMap(),
+        controller: snappingSheetController,
+          widgetBackground: Stack(
+          children: [
+            GoogleSimpleMap(),
+            SafeArea(child: 
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: SearchBar(
+                  handleOnTap: _handleOnTapSearhBar,
+                ),
+              ),
+            )
+          ],
+        )
       ),
       drawer: const DrawerMenu(),
     );
