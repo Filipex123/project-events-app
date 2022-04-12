a9a12c852264e36c576d05ab608014f28ea1d8f1
Filipe Neves <filipe.nevser@gmail.com>
2022-04-12 16:51:19 -0300

    add validations functions

---
+++ lib/utils/validations/new_event_validation.dart
@@ -0,0 +1,35 @@
+import 'dart:developer';
+
+import 'package:brota_ai_app/models/event_model.dart';
+import 'package:brota_ai_app/models/signup_model.dart';
+import 'package:brota_ai_app/utils/regexp.dart';
+import 'package:brota_ai_app/utils/validations/validation.dart';
+import 'package:flutter/foundation.dart';
+
+class NewEventValidation extends Validation {
+  List<String> validate(EventRequestModel input) {
+    List<String> returnErrors = [];
+
+    if(!requiredField(input.name)){
+      returnErrors.add('Campo nome do evento vazio');
+    }
+
+    if(!requiredField(input.sport)){
+      returnErrors.add('Campo esporte vazio');
+    }
+
+    if(!requiredField(input.locale)){
+      returnErrors.add('Campo localização vazio');
+    }
+
+    if(!requiredFieldDate(input.initialDateTime)){
+      returnErrors.add('Campo de data e hora vazio');
+    }
+
+    if(!requiredField(input.sex)){
+      returnErrors.add('Campo de gênero vazio');
+    }
+
+    return returnErrors.map((item) => '- ' + item).toList();
+  }
+}
