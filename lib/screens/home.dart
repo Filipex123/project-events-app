e9ce3642266502cb76c19b5dcf6bb91d4e049846
Filipe Neves <filipe.nevser@gmail.com>
2022-05-24 17:25:37 -0300

    add open event screen on tap in map marker

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,11 +5,14 @@ import 'package:brota_ai_app/components/address_search.dart';
 import 'package:brota_ai_app/components/drawer.dart';
 import 'package:brota_ai_app/components/home_search_bar.dart';
 import 'package:brota_ai_app/components/snapping-sheet/snapping_sheet_widget.dart';
+import 'package:brota_ai_app/screens/join_event_screen.dart';
 import 'package:brota_ai_app/services/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_place/google_place.dart';
 import 'package:snapping_sheet/snapping_sheet.dart';
+import '../models/event_card_model.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String id = 'home_screen';
@@ -68,7 +71,11 @@ class _HomeScreenState extends State<HomeScreen> {
           markerId: markerId,
           position: LatLng(double.parse(element.locale!['lat']), double.parse(element.locale!['lng'])),
           infoWindow: InfoWindow(title: markerIdVal, snippet: element.sport!.name !+ ": " + (element.description ?? '') , onTap: () {
-            log('clicou na info window');
+            GetIt.I<EventResponseCardModel>().locale = element.locale;
+            GetIt.I<EventResponseCardModel>().description = element.description;
+            GetIt.I<EventResponseCardModel>().id = element.id;
+            GetIt.I<EventResponseCardModel>().name = element.name;
+            Navigator.pushNamed(context, JoinEventScreen.id);
           }),
           onTap: () {
             // _onMarkerTapped(markerId);
@@ -82,25 +89,6 @@ class _HomeScreenState extends State<HomeScreen> {
       final GoogleMapController controller = await _controller.future;
       controller.animateCamera(CameraUpdate.newCameraPosition(_kGoogleNewPosition));
 
-    var markerIdVal = 'aaa';
-    final MarkerId markerId = MarkerId(markerIdVal);
-
-    final Marker marker = Marker(
-      markerId: markerId,
-      position: LatLng(result.geometry!.location!.lat ?? -15.793699, result.geometry!.location!.lng ??  -47.882720),
-      infoWindow: InfoWindow(title: markerIdVal, snippet: 'zxxc', onTap: () {
-        log('clicou na info window');
-      }),
-      onTap: () {
-        // _onMarkerTapped(markerId);
-      },
-    );
-
-    // setState(() {
-    //   // adding a new marker to map
-    //   markers[markerId] = marker;
-    //   markers.addAll(marker);
-    // });
     }
   }
 
